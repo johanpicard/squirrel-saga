e0da1790cbaa4e3f7d0743e03408d079876b4b0b
Johan <johan.picard@insa-lyon.fr>
2015-01-16 21:05:44 +0100

    Modèle d'ecureuil et quete modifié

--- app/src/main/java/com/squirrelsaga/modele/Ecureuil.java
+++ app/src/main/java/com/squirrelsaga/modele/Ecureuil.java
@@ -2,17 +2,75 @@ package com.squirrelsaga.modele;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Ecureuil {
 
-        private String nom;
-        private String type;
-        private Map<String,Integer> competences;
-        private ArrayList<Salle> nid;
-        private int nbNoisettes;
+    private String nom;
+    private String cheminImage;
+    private Map<String,Integer> competences;
+    private int nbNoisettes;
 
-        public void mange(int nbNoisettes){
-            this.nbNoisettes += nbNoisettes;
-        }
+
+    public Ecureuil(String nom, String cheminImage) {
+        this.nom = nom;
+        this.cheminImage = cheminImage;
+        competences = new HashMap<String,Integer>();
+        competences.put("Force", 0);
+        competences.put("Intelligence",0);
+        competences.put("Agilite",0);
+    }
+
+    public void mange(int nbNoisettes){
+        this.nbNoisettes += nbNoisettes;
+    }
+
+    public Map<String, Integer> getCompetences() {
+        return competences;
+    }
+
+    public int getForce() {
+        return competences.get("Force");
+    }
+
+    public int getIntelligence() {
+        return competences.get("Intelligence");
+    }
+
+    public int getAgilite() {
+        return competences.get("Agilite");
+    }
+
+    public int forceLevelUp() {
+        return competences.put("Force", competences.get("Force") + 1);
+    }
+
+    public int intelligenceLevelUp() {
+        return competences.put("Intelligence", competences.get("Intelligence") + 1);
+    }
+
+    public int agiliteLevelUp() {
+        return competences.put("Agilite", competences.get("Agilite") + 1);
+    }
+
+    public int getNbNoisettes() {
+        return nbNoisettes;
+    }
+
+    public String getCheminImage() {
+        return cheminImage;
+    }
+
+    public void setCheminImage(String cheminImage) {
+        this.cheminImage = cheminImage;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
--- app/src/main/java/com/squirrelsaga/modele/Quete.java
+++ app/src/main/java/com/squirrelsaga/modele/Quete.java
@@ -1,8 +1,12 @@
 package com.squirrelsaga.modele;
 
 import com.orm.SugarRecord;
+import com.squirrelsaga.controleur.Controleur;
 
+import java.util.HashMap;
 import java.util.Map;
+
+
 
 /**
  * Created by lbillon on 1/14/2015.
@@ -12,8 +16,9 @@ public class Quete extends SugarRecord<Quete> {
 
     private Quete prerequis;
     private Map<String,Integer> competencesRequises;
-    private boolean dispo;
+    private boolean termine;
     private String texte;
+
 
     public double latitude;
     public double longitude;
@@ -21,11 +26,30 @@ public class Quete extends SugarRecord<Quete> {
     public Quete(){
     }
 
-    public Quete(String titre, double latitude, double longitude){
+    public boolean estDispo() {
+        boolean competencesOK = true;
+        Map<String,Integer> competences = Controleur.getEcureuil().getCompetences();
+
+        for (Map.Entry<String, Integer> entry : competencesRequises.entrySet()) {
+           if (competences.get(entry.getKey())< entry.getValue()) {
+               competencesOK = false;
+           }
+        }
+        return !termine && competencesOK && prerequis.estTermine();
+    }
+
+    public Quete(String titre, double latitude, double longitude, int forceRequise, int agiliteRequise, int intelligenceRequise ){
         this.titre = titre;
         this.latitude = latitude;
         this.longitude = longitude;
+        competencesRequises = new HashMap<String,Integer>();
+        competencesRequises.put("Force", forceRequise);
+        competencesRequises.put("Intelligence",agiliteRequise);
+        competencesRequises.put("Agilite",intelligenceRequise);
+    }
 
+    public boolean estTermine() {
+        return termine;
     }
 }
 
