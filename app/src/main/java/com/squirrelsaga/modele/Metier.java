b41a3b10c78c546a4b55c6a79b4d0cf06f458c4f
Johan <johan.picard@insa-lyon.fr>
2015-01-16 16:58:06 +0100

    Quelques modifs du modèee

--- app/src/main/java/com/squirrelsaga/modele/Metier.java
+++
@@ -1,9 +0,0 @@
-package com.squirrelsaga.modele;
-
-
-public class Metier {
-
-        private String nom;
-        private Object bonus;
-
-}
--- app/src/main/java/com/squirrelsaga/modele/Quete.java
+++ app/src/main/java/com/squirrelsaga/modele/Quete.java
@@ -2,11 +2,18 @@ package com.squirrelsaga.modele;
 
 import com.orm.SugarRecord;
 
+import java.util.Map;
+
 /**
  * Created by lbillon on 1/14/2015.
  */
 public class Quete extends SugarRecord<Quete> {
     public String titre;
+
+    private Quete prerequis;
+    private Map<String,Integer> competencesRequises;
+    private boolean dispo;
+    private String texte;
 
     public double latitude;
     public double longitude;
