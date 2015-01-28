ce73148cf12cd9f960f490ebf58c0e2044437053
Laurent BILLON <laurent.billonc@gmail.com>
2015-01-28 16:38:10 +0100

    Refactor Vue_Quete_Vitesse.java

--- app/src/main/java/com/squirrelsaga/vue/Vue_Quete_Vitesse.java
+++ app/src/main/java/com/squirrelsaga/vue/Vue_Quete_Vitesse.java
@@ -4,7 +4,6 @@ package com.squirrelsaga.vue;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -19,7 +18,6 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squirrelsaga.controleur.Controleur;
 import com.squirrelsaga.modele.Ecureuil;
@@ -27,30 +25,54 @@ import com.squirrelsaga.modele.QueteVitesse;
 
 import java.util.concurrent.TimeUnit;
 
-
+/**
+ * Activité permettant de réaliser les quêtes "Vitesse"
+ */
 public class Vue_Quete_Vitesse extends AbstractQueteActivity implements OnMapReadyCallback {
 
-    TextView textViewTime;
-    TextView textViewDistance;
-    private LocationManager locationManager;
-    private CounterClass timer;
+    private TextView textViewTime;
+    private TextView textViewDistance;
+    private CounterClass questTimer;
 
 
-    @Override
-    /**
-     * Initialise la vue en affichant le timer
-     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quete_vitesse);
-        textViewTime = (TextView)findViewById(R.id.text_time);
-        textViewDistance = (TextView)findViewById(R.id.text_distance);
+        setupViews();
 
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        setupLocationManager();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        configureMapOptions(map);
+        QueteVitesse quete = getActiveQuest();
+        addQuestMarker(map, quete);
+        createAndStartQuestTimer(quete);
+
+        updatePlayerLocation(quete.getLocation());
+    }
+
+
+    /**
+     * Initialise le service de géolocalisation
+     */
+    private void setupLocationManager() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 2, new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                processNewLocation(location);
+                updatePlayerLocation(location);
             }
 
             @Override
@@ -68,55 +90,137 @@ public class Vue_Quete_Vitesse extends AbstractQueteActivity implements OnMapRea
 
             }
         });
-
-
     }
 
-    private void processNewLocation(Location location) {
+
+    /**
+     * Met à jour la position du joueur
+     *
+     * @param location Nouvelle position
+     */
+    private void updatePlayerLocation(Location location) {
         double distance = location.distanceTo(getActiveQuest().getObjectifLocation());
-        textViewDistance.setText(String.format("%.0f",distance)+" m");
-        if(distance < 30){
+        textViewDistance.setText(String.format("%.0f", distance) + " m");
+        if (distance < 30) {
             questSuccess();
         }
     }
 
+    /**
+     * Initialise les composants de la vue
+     */
+    private void setupViews() {
+        setContentView(R.layout.activity_quete_vitesse);
+        textViewTime = (TextView) findViewById(R.id.text_time);
+        textViewDistance = (TextView) findViewById(R.id.text_distance);
+    }
+
+
+    /**
+     * Configure la carte
+     *
+     */
+    private void configureMapOptions(GoogleMap map) {
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setCompassEnabled(true);
+        map.getUiSettings().setMapToolbarEnabled(false);
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(45.7791898, 4.8533830)));
+        map.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    /**
+     * Démarre le compte à rebours
+     *
+     */
+    private void createAndStartQuestTimer(QueteVitesse quete) {
+        questTimer = new CounterClass(quete.getObjectifTemps() * 1000);
+        questTimer.start();
+    }
+
+    /**
+     * Ajoute un marqueur de quête à la carte
+     *
+     */
+    private void addQuestMarker(GoogleMap map, QueteVitesse quete) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(quete.latitude, quete.longitude))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_speed)));
+    }
+
+
+    /**
+     * Debug method for testing victory
+     *
+     */
+    @SuppressWarnings("UnusedParameters")
     public void testShortcut(View view) {
         questSuccess();
     }
 
+    /**
+     * Le joueur à gagné
+     */
     private void questSuccess() {
-        timer.cancel();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Tu as réussi la quête dans le temps imparti !")
-                .setTitle("Bravo !")
-                .setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                finish();
-            }
-        }).setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                finish();
-            }
-        });
+        cancelTimer();
 
+
+        updateSquirrelAttributes();
+
+        displaySuccessDialogAndClose();
+
+
+    }
+
+    /**
+     * Arrêt du compte à rebours
+     */
+    private void cancelTimer() {
+        questTimer.cancel();
+    }
+
+    /**
+     * Met à jour les statistiques de l'écureuil
+     */
+    private void updateSquirrelAttributes() {
         QueteVitesse quete = getActiveQuest();
         Ecureuil ecureuil = Controleur.getEcureuil();
         ecureuil.setAReussi(quete.getQueteId());
         ecureuil.mange(quete.getNoisette());
         ecureuil.vitesseLevelUp(quete.getRecompense());
         ecureuil.save();
-        
+    }
+
+    private void displaySuccessDialogAndClose() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Tu as réussi la quête dans le temps imparti !")
+                .setTitle("Bravo !")
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                    }
+                }).setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                finish();
+            }
+        });
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
 
+    /**
+     * Le joueur a perdu
+     */
+    private void questFailure() {
+        cancelTimer();
+        displayFailureDialogAndClose();
 
 
     }
 
-    private void questFailure() {
-        timer.cancel();
+    private void displayFailureDialogAndClose() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Tu n'as pas été assez rapide.")
                 .setTitle("Oh non !")
@@ -134,65 +238,35 @@ public class Vue_Quete_Vitesse extends AbstractQueteActivity implements OnMapRea
 
         AlertDialog dialog = builder.create();
         dialog.show();
-
-
     }
 
     /**
      * Permet de récupérer la quête sélectionnée par l'utilisateur sur la carte
+     *
      * @return QueteVitesse
      */
-    protected QueteVitesse getActiveQuest() {
+    QueteVitesse getActiveQuest() {
         return QueteVitesse.findById(QueteVitesse.class, queteId);
     }
 
     public class CounterClass extends CountDownTimer {
-        public CounterClass(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
+        public CounterClass(long millisInFuture) {
+            super(millisInFuture, (long) 1000);
         }
+
         @Override
         public void onFinish() {
             questFailure();
         }
+
         @Override
         public void onTick(long millisUntilFinished) {
-            long millis = millisUntilFinished;
-            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
-                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
-                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
             textViewTime.setText(hms);
         }
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-
-        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        map.setMyLocationEnabled(true);
-        map.getUiSettings().setCompassEnabled(true);
-        map.getUiSettings().setMapToolbarEnabled(false);
-        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(45.7791898, 4.8533830)));
-        map.moveCamera(CameraUpdateFactory.zoomTo(15));
-
-        QueteVitesse quete = getActiveQuest();
-        Marker marker = map.addMarker(new MarkerOptions()
-                .position(new LatLng(quete.latitude, quete.longitude))
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_speed)));
-
-        timer = new CounterClass(quete.getObjectifTemps()*1000,1000);
-        timer.start();
-
-
-        processNewLocation(quete.getLocation());
-    }
 }
